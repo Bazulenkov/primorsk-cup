d7aae6a90b53c42715da1e04a50ce2bc94299c88
Bazulenkov <anton@bazul.ru>
2021-03-30 22:56:46 +0300

    .

--- primorsk/settings.py
+++ primorsk/settings.py
@@ -168,7 +168,13 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_USE_TLS = bool(int(os.environ.get("EMAIL_USE_TLS")))
+EMAIL_USE_TLS = bool(
+    int(
+        os.environ.get("EMAIL_USE_TLS")
+        if os.environ.get("EMAIL_USE_TLS")
+        else "0"
+    )
+)
 
 
 #  подключаем движок filebased.EmailBackend
