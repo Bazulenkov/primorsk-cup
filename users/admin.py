d4265e05af6171b272fa65ac48ffb5b3162ef3a9
Bazulenkov <anton@bazul.ru>
2021-06-28 18:40:50 +0300

    Добавил, чтобы в админке отображалась дата рег-ции

--- users/admin.py
+++ users/admin.py
@@ -52,9 +52,9 @@ class UserAdmin(DjangoUserAdmin):
             },
         ),
     )
-    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_display = ("email", "first_name", "last_name", "date_joined", "is_staff")
     search_fields = ("email", "first_name", "last_name")
-    ordering = ("email",)
+    ordering = ("last_name",)
     # list_filter = ("username", "email")
 
 
